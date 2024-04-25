64954d13da9d05af0f48f360ec295f909d1d71b2
minji-gwak <mini0006@naver.com>
2024-04-25 18:53:20 +0000

    test: 백엔드 로그인 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { cookies } from 'next/headers';
+
 export default function Home() {
-  return <h1>This is initial gongsilock frontend</h1>;
+  return (
+    <>
+      <h1>로그인을 해봅시다</h1>
+      <p>{JSON.stringify(Object.fromEntries(cookies()))}</p>
+      <a href="https://8d72-125-184-74-26.ngrok-free.app/login">로그인 테스트</a>
+    </>
+  );
 }
