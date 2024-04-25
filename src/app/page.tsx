24e28ca838d7078f084ae7bd0d66f7a6916b5c41
minji-gwak <mini0006@naver.com>
2024-04-25 18:56:19 +0000

    test: 백엔드 로그인 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h1>로그인을 해봅시다</h1>
       <p>{JSON.stringify(Object.fromEntries(cookies()))}</p>
-      <a href="https://8d72-125-184-74-26.ngrok-free.app/login">로그인 테스트</a>
+      <a href="https://8d72-125-184-74-26.ngrok-free.app/oauth2/authorization/google">로그인 테스트</a>
     </>
   );
 }
