927302985917c98569d400d3bf52d4d3b2121783
minji-gwak <mini0006@naver.com>
2024-05-01 12:23:14 +0000

    test: 백엔드 로그인 연결

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { cookies } from 'next/headers';
+
+export default function Home() {
+  return (
+    <>
+      <h1>로그인을 해봅시다</h1>
+      <p>{JSON.stringify(Object.fromEntries(cookies()))}</p>
+      <a href="https://8d72-125-184-74-26.ngrok-free.app/oauth2/authorization/google">로그인 테스트</a>
+    </>
+  );
+}
